81de64b469a506eb20c7b17c077a5261ebdc18b4
Marcus Bernales <mboperator@gmail.com>
2016-03-23 20:51:58 -0700

    Create Actions completed

--- tests/createActions-test.js
+++ tests/createActions-test.js
@@ -1,28 +1,26 @@
 import { expect, should } from 'chai';
 import createActions from '../src/createActions';
-
+should();
 const mockTransforms = [
   { action: 'MOCK_ONE' },
   { action: 'MOCK_TWO' },
 ];
 
-describe('createActions' () => {
+describe('createActions', () => {
   describe('generated hash', () => {
     const generatedActions = createActions(mockTransforms);
+    console.log(generatedActions);
     const firstKey = Object.keys(generatedActions)[0];
 
     it('should return a hash', () => {
       (typeof generatedActions === 'object').should.equal(true);
-    });
-    it('generates functions with a 1 arity', () => {
-      (generatedActions[firstKey].length).should.equal(1);
     });
     it('contain keys that are camelcased', () => {
       firstKey.should.equal('mockOne');
     });
     it('generates the same number of actions as transformations', () => {
       const numberOfActions = Object.keys(generatedActions).length;
-      numberOfActions.length.should.equal(transformations.length);
+      numberOfActions.should.equal(mockTransforms.length);
     });
   });
 });
