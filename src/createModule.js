74d64239a44d91c2b7c311cef185826edac3fa2a
Marcus Bernales <mboperator@gmail.com>
2016-04-25 22:08:25 -0700

    Added createConstants function and corresponding tests

--- src/createModule.js
+++ src/createModule.js
@@ -1,6 +1,7 @@
 import { compose } from 'ramda';
 import createActions from './createActions';
 import createReducer from './createReducer';
+import createConstants from './createConstants';
 import formatConstants from './formatConstants';
 import { Map } from 'immutable';
 
@@ -8,6 +9,7 @@ const _generateReduxComponents = initialState => transformations => {
   const generated = {
     actions: createActions(transformations),
     reducer: createReducer(initialState, transformations),
+    constants: createConstants(transformations),
   };
 
   return generated;
---
+++ tests/createConstants-test.js
@@ -0,0 +1,25 @@
+import { expect, should } from 'chai';
+import createConstants from '../src/createConstants';
+should();
+
+const mockTransforms = [
+  { action: 'MOCK_ONE', formattedConstant: 'mock/MOCK_ONE' },
+  { action: 'MOCK_TWO', formattedConstant: 'mock/MOCK_ONE' },
+];
+
+describe('createConstants', () => {
+  describe('generated hash', () => {
+    const generatedConstants = createConstants(mockTransforms);
+    const firstKey = Object.keys(generatedConstants)[0];
+
+    it('should return a hash', () => {
+      (typeof generatedConstants === 'object').should.equal(true);
+    });
+    it('contains keys that are camelcased', () => {
+      firstKey.should.equal('mockOne');
+    });
+    it('should map formatted constant to action name', () => {
+      generatedConstants[firstKey].should.equal('mock/MOCK_ONE');
+    });
+  });
+});
