9eadaa47096201996fd3c58cca26534819c88405
Marcus Bernales <mboperator@gmail.com>
2016-05-05 20:41:41 -0700

    Added tests around combineNamespacedProps

--- src/connectModule/combineNamespacedProps.js
+++ src/connectModule/combineNamespacedProps.js
@@ -1,13 +1,13 @@
 const combineNamespacedProps =
   (ns = '') => (mapStateProps, mapDispatchProps, props) => {
     const generatedProps = {
-      ...mapStateProps,
-      actions: { ...mapDispatchProps },
+      ... mapStateProps,
+      actions: { ... mapDispatchProps },
     };
 
     return ns
-      ? { [ns]: { ...generatedProps }, ...props }
-      : { ...generatedProps, ...props };
+      ? { [ns]: { ... generatedProps }, ... props }
+      : { ... generatedProps, ... props };
   };
 
 export default combineNamespacedProps;
